46131d4849d13c4ec49fb4ceae2242ba854e4358
Abhishek Nair <abhisheknair1441@gmail.com>
2025-01-18 13:09:22 +0530

    backend: initialized repo and created authroutes

---
+++ backend/src/index.js
@@ -0,0 +1,9 @@
+import express from "express";
+import "dotenv/config";
+import authRoutes from "./routes/auth.route.js";
+const app = express();
+const PORT = process.env.PORT;
+app.use("/api/auth", authRoutes);
+app.listen(PORT, () => {
+  console.log("listening to port", PORT);
+});
